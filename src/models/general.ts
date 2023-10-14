24fd83f95fd4fdadfb122de1815fa5221cacd975
iOSonntag <dev@iosonntag.com>
2023-10-14 16:52:48 +0200

    feat: add StackFunction default value

--- src/models/general.ts
+++ src/models/general.ts
@@ -1,4 +1,4 @@
 import { Stack } from 'sst/constructs';
 import { StackContext } from 'sst/constructs';
 
-export type StackFunction<T> = (this: Stack, ctx: StackContext) => T | Promise<T>;
+export type StackFunction<T = void> = (this: Stack, ctx: StackContext) => T | Promise<T>;
