881b8a48e77c7ec2ac02b6133a2833375cf48957
iOSonntag <dev@iosonntag.com>
2024-03-15 13:03:53 +0100

    feat: add api issue error class

--- src/api-code/models.ts
+++ src/api-code/models.ts
@@ -9,3 +9,25 @@ export type JsonResponse = {
 
 
 export type RestResponse = JsonResponse;
+
+
+
+export type ApiIssueParams = {
+  message: string;
+  error: Error;
+};
+/**
+ * When throwing this error, the error will be logged special and an
+ * notification will be sent to the developers. Afterwards the given error will
+ * be rethrown.
+ */
+export class ApiIssue extends Error
+{
+  public readonly error: Error;
+
+  constructor(params: ApiIssueParams)
+  {
+    super(params.message);
+    this.error = params.error;
+  }
+}
