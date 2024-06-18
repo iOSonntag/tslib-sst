b72911af89b1b82f05d7741688efc7bc89237e55
iOSonntag <dev@iosonntag.com>
2024-06-18 23:50:42 +0200

    feat: convert dynamodb conditional failed check exception

--- src/api-code/throw-utilities/responses.ts
+++ src/api-code/throw-utilities/responses.ts
@@ -1,4 +1,6 @@
 import { ApiResponse, CommonApiResponseCode, CommonApiResponses } from '../responses/api-responses';
+import { ConditionalCheckFailedException } from '@aws-sdk/client-dynamodb';
+import { ElectroError } from 'electrodb';
 
 
 export class ApiResponseThrowable {
@@ -23,4 +25,28 @@ export class ApiResponseThrowable {
 export const throwResponse = (response: ApiResponse | CommonApiResponseCode): never =>
 {
   throw new ApiResponseThrowable(response);
+}
+
+/**
+ * In case of a DynamoDB conditional check failed error, the given response will
+ * be thrown, otherwise the error will be rethrown.
+ */
+export const throwResponseIfDynamoDBConditionalCheckFailed = (e: any, response: ApiResponse | CommonApiResponseCode): never =>
+{
+  if (e instanceof ConditionalCheckFailedException)
+  {
+    throw new ApiResponseThrowable(response);
+  }
+
+  if (e instanceof ElectroError && e.cause instanceof ConditionalCheckFailedException)
+  {
+    throw new ApiResponseThrowable(response);
+  }
+
+  if (e.code === 'ConditionalCheckFailedException')
+  {
+    throw new ApiResponseThrowable(response);
+  }
+
+  throw e;
 }
