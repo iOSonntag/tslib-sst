df911a9824e7f3590573b0f4d2601207160f2d64
iOSonntag <dev@iosonntag.com>
2024-08-30 17:09:14 +0200

    fix: remove todos

--- src/models/general.ts
+++ src/models/general.ts
@@ -1,7 +1,4 @@
 
-export type StackFunction<T = void> = (this: any/*Stack*/, ctx: any/*StackContext*/) => T | Promise<T>;
-
-// TODO: remove this file
 
 
 export const ZERO_UUID = '00000000-0000-0000-0000-000000000000';
