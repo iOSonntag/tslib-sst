cc64aced5094047f7c2adebc7fcd4f6a25ef5446
iOSonntag <dev@iosonntag.com>
2024-10-29 16:26:17 +0100

    feat: add cognito update attribute methods

--- src/api-code/framework-tools/aws/cognito.ts
+++ src/api-code/framework-tools/aws/cognito.ts
@@ -1,7 +1,6 @@
-import { AdminInitiateAuthCommand, AdminSetUserPasswordCommand, AdminUpdateUserAttributesCommand, AliasExistsException, CognitoIdentityProviderClient, InitiateAuthCommand, NotAuthorizedException, RespondToAuthChallengeCommand, AdminDeleteUserCommand } from '@aws-sdk/client-cognito-identity-provider';
-import crypto from 'crypto';
-import { throwResponse } from '../../throw-utilities/responses';
+import { AdminDeleteUserCommand, AdminGetUserCommand, AdminInitiateAuthCommand, AdminSetUserPasswordCommand, AdminUpdateUserAttributesCommand, AliasExistsException, AttributeType, CognitoIdentityProviderClient, NotAuthorizedException } from '@aws-sdk/client-cognito-identity-provider';
 import { Dev } from 'src/api-code/utils/dev';
+import { throwResponse } from '../../throw-utilities/responses';
 
 export * as CognitoService from './cognito';
 
@@ -39,8 +38,6 @@ export const authenticate = async (params: AuthenticateParams): Promise<Authenti
     });
 
     const authResponse = await client.send(initiateAuthCommand);
-
-    client.destroy();
   
     if (!authResponse.AuthenticationResult)
     {
@@ -65,8 +62,6 @@ export const authenticate = async (params: AuthenticateParams): Promise<Authenti
   }
   catch (error)
   {
-    client.destroy();
-
     if (error instanceof NotAuthorizedException)
     {
       return {
@@ -79,7 +74,126 @@ export const authenticate = async (params: AuthenticateParams): Promise<Authenti
 
     throw error;
   }
+  finally
+  {
+    client.destroy();
+  }
 }
+
+
+export type UpdateUserAttributeParams = {
+  userPoolId: string;
+  clientId: string;
+  region: string;
+  username: string;
+  attributeName: string;
+  attributeValue: string;
+};
+
+export const updateUserAttribute = async (params: UpdateUserAttributeParams): Promise<void> =>
+{
+  const client = new CognitoIdentityProviderClient({ 
+    region: params.region,
+    logger: Dev.awsLogger
+  });
+
+  try
+  {
+    const updateCommand = new AdminUpdateUserAttributesCommand({
+      UserPoolId: params.userPoolId,
+      Username: params.username,
+      UserAttributes: [
+        {
+          Name: params.attributeName,
+          Value: params.attributeValue
+        }
+      ]
+    });
+
+    await client.send(updateCommand);
+  }
+  catch (error)
+  {
+    Dev.logIssue('Failed to update attribute', error);
+
+    throw error;
+  }
+  finally
+  {
+    client.destroy();
+  }
+}
+
+
+
+export type GetUserAttributesParams = {
+  userPoolId: string;
+  clientId: string;
+  region: string;
+  username: string;
+};
+
+export const getUserAttributes = async (params: GetUserAttributesParams): Promise<AttributeType[]> =>
+{
+  const client = new CognitoIdentityProviderClient({ 
+    region: params.region,
+    logger: Dev.awsLogger
+  });
+
+  try
+  {
+    const getCommand = new AdminGetUserCommand({
+      UserPoolId: params.userPoolId,
+      Username: params.username
+    });
+
+    const response = await client.send(getCommand);
+
+    if (!response.UserAttributes)
+    {
+      Dev.logIssue(`No cognito attributes found for user ${params.username}`);
+      return [];
+    }
+
+    return response.UserAttributes;
+  }
+  catch (error)
+  {
+    Dev.logIssue('Failed to get attribute', error);
+
+    throw error;
+  }
+  finally
+  {
+    client.destroy();
+  }
+}
+
+
+export type GetUserAttributeParams = {
+  userPoolId: string;
+  clientId: string;
+  region: string;
+  username: string;
+  attributeName: string;
+};
+
+export const getUserAttribute = async (params: GetUserAttributeParams): Promise<string | undefined> =>
+{
+  const attributes = await getUserAttributes(params);
+
+  const attribute = attributes.find(a => a.Name === params.attributeName);
+
+  if (!attribute)
+  {
+    return undefined;
+  }
+
+  return attribute.Value;
+}
+
+
+
 
 
 export type UpdateUserEmailParams = {
@@ -121,14 +235,9 @@ export const updateUserEmail = async (params: UpdateUserEmailParams): Promise<vo
     });
 
     await client.send(updateCommand);
-
-    client.destroy();
   }
   catch (error)
   {
-
-    client.destroy();
-
     if (error instanceof AliasExistsException)
     {
       throw throwResponse('RESOURCE_ALREADY_EXISTS');
@@ -137,6 +246,10 @@ export const updateUserEmail = async (params: UpdateUserEmailParams): Promise<vo
     Dev.logIssue('Failed to update email', error);
 
     throw error;
+  }
+  finally 
+  {
+    client.destroy();
   }
 }
 
@@ -165,16 +278,16 @@ export const updatePassword = async (params: UpdatePasswordParams): Promise<void
     });
 
     await client.send(updateCommand);
-
-    client.destroy();
   }
   catch (error)
   {
-    client.destroy();
-
     Dev.logIssue('Failed to update password', error);
 
     throw error;
+  }
+  finally
+  {
+    client.destroy();
   }
 }
 
@@ -200,15 +313,15 @@ export const deleteUser = async (params: DeleteUserParams): Promise<void> =>
     });
 
     await client.send(deleteCommand);
-
-    client.destroy();
   }
   catch (error)
   {
-    client.destroy();
-
     Dev.logIssue('Failed to delete user', error);
 
     throw error;
+  }
+  finally
+  {
+    client.destroy();
   }
 }
